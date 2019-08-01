79d3b650627ffb51f235241e1fe503ac3370a6a4
用joker <2101418065@qq.com>
2019-08-01 13:53:26 +0800

    springboot通过注解方式整合Filter

--- filter/src/main/java/com/joker/application/FilterApplication.java
+++ filter/src/main/java/com/joker/application/FilterApplication.java
@@ -1,16 +1,14 @@
 package com.joker.application;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ServletComponentScan("com.joker")
-
-public class Application {
+public class FilterApplication {
   public static void main(String[] args) {
-    SpringApplication.run(Application.class,args);
+    SpringApplication.run(FilterApplication.class,args);
   }
 }
---
+++ filter/src/main/java/com/joker/filter/FirstFilter.java
@@ -0,0 +1,29 @@
+package com.joker.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * Springboot整合Filter方式一
+ */
+
+@WebFilter(filterName = "FirstFilter",urlPatterns = "/boot1")
+public class FirstFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("进入Filter");
+        filterChain.doFilter(servletRequest,servletResponse);
+        System.out.println("离开filter");
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
