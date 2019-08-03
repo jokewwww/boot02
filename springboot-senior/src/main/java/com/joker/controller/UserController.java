77be10dc2f3436a6cba77c0e61aac490e50d28d3
用joker <2101418065@qq.com>
2019-08-03 10:06:45 +0800

    springboot校验:1.在Controller中添加校验规则

--- springboot-senior/src/main/java/com/joker/controller/UserController.java
+++ springboot-senior/src/main/java/com/joker/controller/UserController.java
@@ -3,8 +3,11 @@ package com.joker.controller;
 
 import com.joker.pojo.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
@@ -12,8 +15,18 @@ public class UserController {
     @RequestMapping("{page}")
     public String showPagr(@PathVariable String page){return page;}
 
+
+    /**
+     * @param user 添加@Valid:开启堆User对象的数据校验
+     * @param result @Valid 开启堆User对象的数据校验
+     * @return
+     */
     @RequestMapping("save")
-    public String saveUser(User user){
+    public String saveUser(@Valid User user, BindingResult result){
+        if (result.hasErrors()){
+            return "add";
+        }
+
         return "ok";
     }
 
